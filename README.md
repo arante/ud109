83d9688ee73fad5c7719c0bc700e1c5a5855e7c9
tuittQuimpo <quimpo.charles@gmail.com>
2018-05-22 15:19:12 +0800

    Lesson 3: Write the Fetch Request

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Asynchronous JavaScript Request (ud109)
 
+## Notes
+
+## Resources
+
 ---
 Created by [Billy Wilson A. Arante](https://arante.github.io/portfolio/) of Mandaluyong City, Philippines
---
+++ lesson3/assets/js/script.js
@@ -0,0 +1,4 @@
+/* Writing the fetch request */
+/* Will cause a CORS request error */
+/* Load this via DevTools console instead */
+fetch('`https://api.unsplash.com/search/photos?page=1&query=flowers`');
