a52c0405ae62b82015983c9977082131f7c539eb
arante <arantebillywilson@gmail.com>
2018-05-22 15:32:20 +0800

    Update: Lesson 3 - Handle the response

--- lesson3/assets/js/script.js
+++ lesson3/assets/js/script.js
@@ -1,4 +1,11 @@
 /* Writing the fetch request */
 /* Will cause a CORS request error */
 /* Load this via DevTools console instead */
-fetch('`https://api.unsplash.com/search/photos?page=1&query=flowers`');
+// fetch('https://api.unsplash.com/search/photos?page=1&query=flowers');
+
+let searchedForText = 'flowers';
+fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+	headers: {
+		Authorization: 'Client-ID abc123'
+	}
+});
